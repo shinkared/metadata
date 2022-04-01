c526e117394ba6d94430dada1ce011983f3a8313
Win10_Game_OS <Win10_Game_OS@DESKTOP-TSC2GBE>
2022-04-01 14:53:49 +0300

    Добавьте файлы проекта.

---
+++ metadata/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Win10_Game_OS\source\repos\metadata\metadata\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
